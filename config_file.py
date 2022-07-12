8e5f5327408a50c9a9cfe8fc159e1177e75bcc07
liaafk <kusumastutiaurelia@gmail.com>
2022-07-12 14:16:15 +0200

    fixed "10 points" bug

--- config_file.py
+++ config_file.py
@@ -1,6 +1,6 @@
-QUERY = ["SELECT ST_ENVELOPE(loc) FROM orders_de WHERE minutes >= 60"]
+QUERY = ["SELECT ST_ENVELOPE(loc) FROM orders_de LIMIT 5"]
 DATAPOINT_ATTRIBUTE = 'loc'
-EPSILON = [1.0, 2.0, 3.0, 4.0, 5.0]
-REMOVE_EXTREME_POINTS = [True, False]
-NOISY_POINTS = [True, False]
-NOISY_RESULT = [True, False]
+EPSILON = [1.0]
+REMOVE_EXTREME_POINTS = [True]
+NOISY_POINTS = [True]
+NOISY_RESULT = [True]
