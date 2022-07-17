94b6b2f5f1676f83676974599a238a18777a14ca
Lotta Fagel <lotta.fagel@kv.digital>
2022-07-17 12:31:31 +0200

    cleanup added comments

--- performance_test.py
+++ performance_test.py
@@ -1,17 +1,12 @@
 import time
 import csv
-from geo_dp_functions import getQueryParts, noisy_sql_response
+from geo_dp_functions import noisy_sql_response
 import geopandas as gpd
 import psycopg2
-#import pydp as dp
 from configparser import ConfigParser
-from geo_dp_functions import noisy_sql_response, getNoisyPoints, getQueryPoints, testquery
+from geo_dp_functions import noisy_sql_response
 import matplotlib.pyplot as plt
 import numpy as np
-from itertools import product
-import sys
-import config_file
-from shapely import geometry
 
 def config(filename='database.ini', section='postgresql'):
     # create a parser
@@ -32,7 +27,7 @@ def config(filename='database.ini', section='postgresql'):
 
 
 def connect():
-    """ Connect to the PostgreSQL database server """
+    # Connect to the PostgreSQL database server 
     conn = None
     try:
         # read connection parameters
@@ -45,25 +40,29 @@ def connect():
         # create a cursor
         cur = conn.cursor()
 
+        # setting test parameters
         epsilon = 100
         datapoint_attribute = 'loc'
         remove_extreme_points = False
         noisy_points = True
         noisy_result = True
+
+        # reading the query lists
         with open('query_list.csv', newline='') as f:
             reader = csv.reader(f)
-            query_list = [x[0] for x in reader]#list(reader)
-            #print(query_list)
+            query_list = [x[0] for x in reader]
         with open('query_list_postgis.csv', newline='') as f:
             reader = csv.reader(f)
-            query_list_postgis = [x[0] for x in reader]#list(reader)
-            #print(query_list_postgis)
-
-        noisy_responses, responses = compare_noise_without(query_list, query_list_postgis, datapoint_attribute, conn, epsilon, remove_extreme_points, noisy_points, noisy_result)
-        print("Comparison noise or without noise: ", noisy_responses, responses)
+            query_list_postgis = [x[0] for x in reader]
         
+        # comparing query execution time with and without noise
+        noisy_responses, responses = compare_noise_without(query_list, query_list_postgis, datapoint_attribute, conn, epsilon, remove_extreme_points, noisy_points, noisy_result)
+        print("Comparison noise and without noise result: ", noisy_responses, responses)
+        
+        # comparing the option of noising each point, noising the result and doing both
         response_extreme, response_points, response_result = compare_noise_options(query_list, datapoint_attribute, conn, epsilon)
-        print("Options result: ", response_extreme, response_points, response_result)
+        print("Comparison of options result: ", response_extreme, response_points, response_result)
+
 	# close the communication with the PostgreSQL
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
@@ -74,15 +73,16 @@ def connect():
             print('Database connection closed.')
     return noisy_responses, responses
 
+# comparing the results of queries with and without noise
 def compare_noise_without(query_list, query_list_postgis, datapoint_attribute, conn, epsilon, remove_extreme_points, noisy_points, noisy_result):
         meantime_noisy_list = []
         meantime_wo_noise_list = []
-        #execute all queries of query_list with noise
+        # executing all queries of query_list with noise
         noisy_responses = []
+        print("*****************RESPONES WITH NOISE*****************")
         for query in query_list:
-            print("********************QUERY****************************")
             print(query)
-            #execute each query 10 times and calculate the mean
+            # executing each query 1000 times and calculate the mean
             for l in range(1, 1001):
                 time_noisy = 0
                 start = time.time()
@@ -97,16 +97,15 @@ def compare_noise_without(query_list, query_list_postgis, datapoint_attribute, c
         print("meantime_noisy_list: ", meantime_noisy_list)
         print("**********************************************************")
 
-        #execute all queries of query_list without noise
+        # executing all queries of query_list_postgis without noise
         responses = []
+        print("*****************RESPONES WITHOUT NOISE*****************")
         for query in query_list_postgis:
-            print("********************QUERY****************************")
             for l in range (1, 1001):
                 time_wo_noise = 0
                 start = time.time()
                 type_select = query.split()[1].lower().split('(')[0]
                 response = gpd.read_postgis(query, conn, geom_col=type_select)
-                #response = gpd.GeoDataFrame.from_postgis(query, conn)#, geom_col=datapoint_attribute)
                 end = time.time()
                 t = end - start
                 time_wo_noise += t
@@ -117,16 +116,15 @@ def compare_noise_without(query_list, query_list_postgis, datapoint_attribute, c
         print("meantime_wo_noise_list: ", meantime_wo_noise_list)
         print("**********************************************************")
 
+        # putting results into a diagram
         labels = ['BB100', 'BB1000', 'Centroid100', 'Centroid1000', 'Union100', 'Union1000']
-
-        x = np.arange(len(labels))  # the label locations
-        width = 0.35  # the width of the bars
+        x = np.arange(len(labels))
+        width = 0.35
         plt.xticks(rotation=45)
         fig, ax = plt.subplots()
         rects1 = ax.bar(x - width/2, meantime_noisy_list, width, label='With noise')
         rects2 = ax.bar(x + width/2, meantime_wo_noise_list, width, label='Without noise')
 
-        # Add some text for labels, title and custom x-axis tick labels, etc.
         ax.set_ylabel('Execution time')
         ax.set_title('With and without noise')
         ax.set_xticks(x, labels)
@@ -139,6 +137,7 @@ def compare_noise_without(query_list, query_list_postgis, datapoint_attribute, c
         plt.show()
         return noisy_responses, responses
 
+# comparing the option of noising each point, noising the result and doing both
 def compare_noise_options(query_list, datapoint_attribute, conn, epsilon):
         meantime_delete_extreme = []
         meantime_noisy_points = []
@@ -150,8 +149,8 @@ def compare_noise_options(query_list, datapoint_attribute, conn, epsilon):
         for query in query_list[:2]:
             print("********************QUERY****************************")
             print(query)
-            #execute each query 10 times and calculate the mean
-            for l in range(1, 31):
+            #execute each query 1000 times and calculate the mean
+            for l in range(1, 1001):
                 #only deleting extreme points
                 time_extreme = 0
                 start = time.time()
@@ -177,15 +176,15 @@ def compare_noise_options(query_list, datapoint_attribute, conn, epsilon):
                 time_result += t
 
             noisy_responses_extreme.append(response_extreme)
-            meantime_extreme = time_extreme/30
+            meantime_extreme = time_extreme/1000
             meantime_delete_extreme.append(meantime_extreme)
 
             noisy_responses_points.append(response_points)
-            meantime_points = time_points/30
+            meantime_points = time_points/1000
             meantime_noisy_points.append(meantime_points)
 
             noisy_responses_result.append(response_result)
-            meantime_result = time_result/30
+            meantime_result = time_result/1000
             meantime_noisy_result.append(meantime_result)
 
         print("**********************************************************")
@@ -200,17 +199,16 @@ def compare_noise_options(query_list, datapoint_attribute, conn, epsilon):
         print("meantime_noisy_result: ", meantime_noisy_result)
         print("**********************************************************")
 
+        # putting results into a diagram
         labels = ['BB100', 'BB1000']
-
-        x = np.arange(len(labels))  # the label locations
-        width = 0.15  # the width of the bars
+        x = np.arange(len(labels))
+        width = 0.15
         plt.xticks(rotation=45)
         fig, ax = plt.subplots()
         rects1 = ax.bar(x - width, meantime_delete_extreme, width, label='Delete Extreme Points')
         rects2 = ax.bar(x, meantime_noisy_points, width, label='Noise Points')
         rects3 = ax.bar(x + width, meantime_noisy_result, width, label='Noise Result')
 
-        # Add some text for labels, title and custom x-axis tick labels, etc.
         ax.set_ylabel('Execution time')
         ax.set_title('Options to obfuscate data')
         ax.set_xticks(x, labels)
@@ -229,27 +227,3 @@ if __name__ == '__main__':
     noisy_responses, responses = connect()
     print(noisy_responses)
     print(responses)
-
-
-def sql_response(query, datapoint_attribute, conn):
-    #points relevant to query
-    points = getQueryPoints(query, datapoint_attribute, conn)
-    #print("points wo noise: ", points)
-
-    geo_df = gpd.GeoDataFrame(points, crs="EPSG:4326")
-    select_query =  getQueryParts(query)[0].lower()
-    if "st_envelope" in select_query:
-        result = geo_df.dissolve().total_bounds
-           
-    elif "st_centroid" in select_query:
-        result = geo_df.dissolve().centroid[0]
-
-    elif "st_union" in select_query:
-        print(geo_df.longitude)
-        result = geometry.Polygon([[p.x, p.y] for p in geo_df])
-        
-    else:
-        result = ""
-        print("ERROR: No geo spacial method found in SELECT-part.")
-    print("Result: ")
-    return result
