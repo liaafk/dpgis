percentiles deleted average latency bugfix diagram

performance_test.py
@@ -46,7 +46,9 @@ def connect():
         #noisy_points = True
         #noisy_result = True
         #local_dp = True
-        repetitions = 100
+        repetitions = 1
+        # drops the upper and lower percentil, needs to be lower than 0,5
+        drop_percentile = float(0.0)
         # reading the query lists
         with open('query_list.txt', newline='') as f:
             #reader = csv.reader(f)
@@ -63,7 +65,7 @@ def connect():
         #response_points, response_result, response_both_options = compare_noise_options(query_list, datapoint_attribute, conn, epsilon)
         #print("Comparison of options result: ", response_points, response_result, response_both_options)
 
-        noisy_points_responses, noisy_result_responses, noisy_both_responses, ldp_responses, responses = comparison_options(query_list, datapoint_attribute, conn, epsilon, repetitions)
+        noisy_points_responses, noisy_result_responses, noisy_both_responses, ldp_responses, responses = comparison_options(query_list, datapoint_attribute, conn, epsilon, repetitions, drop_percentile)
         #print("Comparison of noisy response and no PostGIS request result: ", response_noisy_2, response_wo_postgis_func)
 
 	# close the communication with the PostgreSQL
@@ -144,7 +146,7 @@ def compare_noise_without(query_list, query_list_postgis, datapoint_attribute, c
         print("**********************************************************")
 
         # putting results into a diagram
-        labels = ['BB100', 'BBr1000', 'Extent100', 'Extent1000', 'Centroid100', 'Centroid1000', 'Union100', 'Union1000']
+        labels = ['Bounding box 100', 'Bounding box 1000', 'Extent 100', 'Extent 1000', 'Centroid 100', 'Centroid 1000', 'Union 100', 'Union 1000']
         x = np.arange(len(labels))
         width = 0.35
         plt.setp
@@ -170,14 +172,14 @@ def compare_noise_options(query_list, datapoint_attribute, conn, epsilon, repeti
         meantime_both_options_list = []
         meantime_noisy_points_list = []
         meantime_noisy_result_list = []
-        #execute all queries of query_list with noise
+        # execute all queries of query_list with noise
         noisy_responses_both = []
         noisy_responses_points = []
         noisy_responses_result = []
         for query in query_list[:2]:
             print("********************QUERY****************************")
             print(query)
-            #execute each query repetitions times and calculate the mean
+            # execute each query repetitions times and calculate the mean
             for l in range(1, repetitions+1):
                 # only noising points 
                 time_points = 0
@@ -228,7 +230,7 @@ def compare_noise_options(query_list, datapoint_attribute, conn, epsilon, repeti
         print("**********************************************************")
 
         # putting results into a diagram
-        labels = ['BB100', 'BBrepetitions']
+        labels = ['Bounding box 100', 'Bounding Box repetitions']
         x = np.arange(len(labels))
         width = 0.25
         plt.setp
@@ -249,27 +251,32 @@ def compare_noise_options(query_list, datapoint_attribute, conn, epsilon, repeti
         plt.show()
         return response_points, response_result, response_both_options
 
-def comparison_options(query_list, datapoint_attribute, conn, epsilon, repetitions):
+def comparison_options(query_list, datapoint_attribute, conn, epsilon, repetitions, drop_percentile):
         avg_time_noisy_points_list = []
         avg_time_noisy_result_list = []
         avg_time_noisy_both_list = []
         avg_time_ldp_list = []
         avg_time_baseline_list = []
-        # executing all queries of query_list with noise
         noisy_points_responses = []
+        drop_lower = int(repetitions*drop_percentile)
+        drop_upper = int(repetitions-(repetitions*drop_percentile))
+        amount_after_drop = int(repetitions-(2*repetitions*drop_percentile))
         print("*****************NOISY POINTS*****************")
+        # executing all queries of query_list with noise
         for query in query_list:
             print(query)
+            time_noisy_points = []
             # executing each query repetitions times and calculate the mean
             for l in range(1, repetitions+1):
-                time_noisy_points = 0
                 start = time.time()
                 response = noisy_sql_response(query, datapoint_attribute, conn, epsilon, True, False, False) 
                 end = time.time()
                 t = end - start
-                time_noisy_points += t
+                time_noisy_points.append(t)
             noisy_points_responses.append(response)
-            avg_time_noisy_points = time_noisy_points/repetitions
+            time_noisy_points.sort()
+            time_noisy_points = time_noisy_points[drop_lower:drop_upper]
+            avg_time_noisy_points = round(sum(time_noisy_points)/amount_after_drop, 4)
             avg_time_noisy_points_list.append(avg_time_noisy_points)
         print("**********************************************************")
         print("avg noisy points: ", avg_time_noisy_points_list)
@@ -279,16 +286,18 @@ def comparison_options(query_list, datapoint_attribute, conn, epsilon, repetitio
         print("*****************NOISY RESULT*****************")
         for query in query_list:
             print(query)
+            time_noisy_result = []
             # executing each query repetitions times and calculate the mean
             for l in range(1, repetitions+1):
-                time_noisy_result = 0
                 start = time.time()
                 response = noisy_sql_response(query, datapoint_attribute, conn, epsilon, False, True, False) 
                 end = time.time()
                 t = end - start
-                time_noisy_result += t
+                time_noisy_result.append(t)
             noisy_result_responses.append(response)
-            avg_time_noisy_result = time_noisy_result/repetitions
+            time_noisy_result.sort()
+            time_noisy_result = time_noisy_result[drop_lower:drop_upper]
+            avg_time_noisy_result = round(sum(time_noisy_result)/amount_after_drop, 4)
             avg_time_noisy_result_list.append(avg_time_noisy_result)
         print("**********************************************************")
         print("avg noisy result: ", avg_time_noisy_result_list)
@@ -298,16 +307,18 @@ def comparison_options(query_list, datapoint_attribute, conn, epsilon, repetitio
         print("*****************NOISY POINTS AND RESULT*****************")
         for query in query_list:
             print(query)
+            time_noisy_both = []
             # executing each query repetitions times and calculate the mean
             for l in range(1, repetitions+1):
-                time_noisy_both = 0
                 start = time.time()
                 response = noisy_sql_response(query, datapoint_attribute, conn, epsilon, True, True, False) 
                 end = time.time()
                 t = end - start
-                time_noisy_both += t
+                time_noisy_both.append(t)
             noisy_both_responses.append(response)
-            avg_time_noisy_both = time_noisy_both/repetitions
+            time_noisy_both.sort()
+            time_noisy_both = time_noisy_both[drop_lower:drop_upper]
+            avg_time_noisy_both = round(sum(time_noisy_both)/amount_after_drop, 4)
             avg_time_noisy_both_list.append(avg_time_noisy_both)
         print("**********************************************************")
         print("avg noisy points and result: ", avg_time_noisy_result_list)
@@ -317,38 +328,37 @@ def comparison_options(query_list, datapoint_attribute, conn, epsilon, repetitio
         print("*****************RESPONSES WITH LDP*****************")
         for query in query_list:
             print(query)
+            time_ldp = []
             # executing each query repetitions times and calculate the mean
             for l in range(1, repetitions+1):
-                time_ldp = 0
                 start = time.time()
                 response = noisy_sql_response(query, datapoint_attribute, conn, epsilon, False, False, True)
                 end = time.time()
                 t = end - start
-                time_ldp += t
+                time_ldp.append(t)
             ldp_responses.append(response)
-            avg_time_ldp = time_ldp/repetitions
+            time_ldp.sort()
+            time_ldp = time_ldp[drop_lower:drop_upper]
+            avg_time_ldp = round(sum(time_ldp)/amount_after_drop, 4)
             avg_time_ldp_list.append(avg_time_ldp)
         print("**********************************************************")
         print("avg_time_ldp_list: ", avg_time_ldp_list)
         print("**********************************************************")
-        
-        #with open('query_list_wo_postgis_func.txt', newline='') as f:
-        #    query_list_wo_postgis_func = f.readlines()
 
         # executing all queries of query_list_wo_postgis_func without noise
         responses = []
+        time_baseline = []
         print("*****************RESPONSES BASELINE*****************")
         for query in query_list:
             for l in range (1, repetitions+1):
-                time_baseline = 0
                 start = time.time()
-                #to_read = query.split()[1].lower()
-                #type_select = to_read.split('(')[0]
                 response = noisy_sql_response(query, datapoint_attribute, conn, epsilon, False, False, False) #gpd.read_postgis(query, conn, geom_col=type_select)
                 end = time.time()
                 t = end - start
-                time_baseline += t
-            avg_time_baseline = time_baseline/repetitions
+                time_baseline.append(t)
+            time_baseline.sort()
+            time_baseline = time_baseline[drop_lower:drop_upper]
+            avg_time_baseline = round(sum(time_baseline)/amount_after_drop, 4)
             avg_time_baseline_list.append(avg_time_baseline)
             responses.append(response)
         print("**********************************************************")
@@ -356,27 +366,27 @@ def comparison_options(query_list, datapoint_attribute, conn, epsilon, repetitio
         print("**********************************************************")
 
         # putting results into a diagram
-        labels = ['BB100', 'BB1000', 'Extent100', 'Extent1000', 'Centroid100', 'Centroid1000', 'Union100', 'Union1000']
+        labels = ['Bounding box 100', 'Bounding box 1000', 'Extent 100', 'Extent 1000', 'Centroid 100', 'Centroid 1000', 'Union 100', 'Union 1000']
         x = np.arange(len(labels))
-        width = 0.35
+        width = 0.18
         plt.setp
-        fig, ax = plt.subplots(figsize=(10,8))
-        rects1 = ax.bar(x - 4*width/5, avg_time_noisy_points_list, width, label='Laplace noise: points')
-        rects2 = ax.bar(x - 2*width/5, avg_time_noisy_points_list, width, label='Laplace noise: result')
-        rects3 = ax.bar(x, avg_time_noisy_points_list, width, label='Laplace noise: points and result')
-        rects4 = ax.bar(x + 2*width/5, avg_time_ldp_list, width, label='LDP')
-        rects5 = ax.bar(x + 4*width/5, avg_time_baseline_list, width, label='Baseline')
-
+        fig, ax = plt.subplots(figsize=(15,8))
+        rects1 = ax.bar(x - 2*width, avg_time_noisy_points_list, width, label='Laplace noise: points')
+        rects2 = ax.bar(x - width, avg_time_noisy_result_list, width, label='Laplace noise: result')
+        rects3 = ax.bar(x, avg_time_noisy_both_list, width, label='Laplace noise: points and result')
+        rects4 = ax.bar(x + width, avg_time_ldp_list, width, label='LDP')
+        rects5 = ax.bar(x + 2*width, avg_time_baseline_list, width, label='Baseline')
+        plt.ylim(0, 0.5)
         ax.set_ylabel('Latency')
         ax.set_title('Performance comparison')
         ax.set_xticks(x, labels)
         ax.legend()
 
-        ax.bar_label(rects1, padding=3)
-        ax.bar_label(rects2, padding=3)
-        ax.bar_label(rects3, padding=3)
-        ax.bar_label(rects4, padding=3)
-        ax.bar_label(rects5, padding=3)
+        ax.bar_label(rects1, padding=5, rotation=45)
+        ax.bar_label(rects2, padding=5, rotation=45)
+        ax.bar_label(rects3, padding=5, rotation=45)
+        ax.bar_label(rects4, padding=5, rotation=45)
+        ax.bar_label(rects5, padding=5, rotation=45)
         fig.autofmt_xdate()
         plt.show()
         return noisy_points_responses, noisy_result_responses, noisy_both_responses, ldp_responses, responses